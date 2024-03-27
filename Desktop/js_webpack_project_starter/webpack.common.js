097c07cfe323a30263a623a41387f1aa00f61c23
Kanon <mkc35r.0@gmail.com>
2024-03-28 01:21:23 +0900

    error 修正（image）

--- Desktop/js_webpack_project_starter/webpack.common.js
+++ Desktop/js_webpack_project_starter/webpack.common.js
@@ -15,13 +15,22 @@ module.exports = {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|ico|gif)$/,
+        type: 'asset/resource',
+        loader: 'image-webpack-loader',
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
-      chunks: 'app',
+      chunks: ['app'],
     }),
     new MiniCssExtractPlugin({
       filename: './css/[name].css',
